327d159cae06e61b7a0f924bebff04929051f77e
andrey_dergunov <andrei_dergunov@epam.com>
2024-08-11 11:57:17 +0200

    ...

--- cmake-build-debug/UnitTests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrey/CLionProjects/argparser/UnitTests
-# Build directory: /home/andrey/CLionProjects/argparser/cmake-build-debug/UnitTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(internal_tests "/home/andrey/CLionProjects/argparser/cmake-build-debug/UnitTests/internal_tests")
-set_tests_properties(internal_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/andrey/CLionProjects/argparser/UnitTests/CMakeLists.txt;9;add_test;/home/andrey/CLionProjects/argparser/UnitTests/CMakeLists.txt;0;")
