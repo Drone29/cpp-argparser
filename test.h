8869d26ae74a2b646adb6314178cbea483df83ac
andrey_dergunov <andrei_dergunov@epam.com>
2024-12-20 20:15:08 +0100

    CompositeFixed

--- test.h
+++ test.h
@@ -42,21 +42,30 @@ public:
             : data(std::move(values)...) {}
 };
 
-// Forward declaration for the builder
-template <typename... Types>
-class CompositeBuilder;
-
-// Base builder type
-class BasicBuilder {
+// composite class with fixed parameters
+template<typename T, size_t STR_ARGS, typename... Targs>
+class CompositeFixed : public Base {
+private:
+    T value; //holds value
+    std::tuple<Targs...> targs; // holds function, side arguments
 public:
-    template <typename A, typename... Keys>
-    auto AddArgument(Keys ...keys) {
-        static_assert(sizeof...(keys) > 0, "Keys' set cannot be empty");
-        static_assert((std::is_same_v<Keys, const char*> && ...), "Keys must be strings");
-        return CompositeBuilder<A>(
-                std::vector<std::string>{keys...},
-                std::make_tuple(A{})
-                ); // create a composite builder
+    explicit CompositeFixed(Targs... vals)
+        : value(),
+        targs(std::move(vals)...){}
+
+    void action(const std::initializer_list<const char*> &args) override {
+        // get number of str args
+        const auto str_args = std::get<1>(targs); // arguments should always be first
+        const auto args_num = std::tuple_size_v<decltype(str_args)>;
+        // func + side args
+        auto func_and_side_args = std::get<2>(targs);
+        auto func = std::get<0>(func_and_side_args); // function
+        auto side_args = std::get<1>(func_and_side_args); // arguments in a tuple
+        std::array<const char*, STR_ARGS> str_arr;
+        std::copy(args.begin(), args.end(), str_arr.begin());
+        auto tpl_res = std::tuple_cat(side_args, str_arr);
+        //call function
+        value = std::apply(func, tpl_res);
     }
 };
 
@@ -68,15 +77,25 @@ private:
 
     template <std::size_t... Is>
     Base* finalizeHelper(std::index_sequence<Is...>) {
-        return new Composite<Types...>(std::get<Is>(components)...);
+//        return new Composite<Types...>(std::get<Is>(components)...);
+        auto val = std::get<0>(components);
+        const size_t tpl_size = std::tuple_size_v<decltype(components)>;
+        if constexpr (tpl_size > 1) {
+            auto str_args = std::get<1>(components);
+            const size_t str_args_size = std::tuple_size_v<decltype(str_args)>;
+            return new CompositeFixed<decltype(val), str_args_size, Types...>(std::get<Is>(components)...);
+        } else {
+            return new CompositeFixed<decltype(val), 0, Types...>(std::get<Is>(components)...);
+        }
+        return nullptr;
     }
+
     template <typename NewType>
     auto add(NewType newComponent) {
         return CompositeBuilder<Types..., NewType>(
                 std::move(keys),
                 std::tuple_cat(components, std::make_tuple(newComponent)));
     }
-
 public:
     // composite constructor
     explicit CompositeBuilder(std::vector<std::string> &&ks, std::tuple<Types...> &&comps)
@@ -104,6 +123,20 @@ public:
 
     Base* Finalize() {
         return finalizeHelper(std::make_index_sequence<sizeof...(Types)>());
+    }
+};
+
+// Base builder type
+class BasicBuilder {
+public:
+    template <typename A, typename... Keys>
+    auto AddArgument(Keys ...keys) {
+        static_assert(sizeof...(keys) > 0, "Keys' set cannot be empty");
+        static_assert((std::is_same_v<Keys, const char*> && ...), "Keys must be strings");
+        return CompositeBuilder<A>(
+                std::vector<std::string>{keys...},
+                std::make_tuple(A{})
+        ); // create a composite builder
     }
 };
 
